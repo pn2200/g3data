99e7155126f1b26353f8b7335acd552c4946d44c
Paul Novak <pnovak@alumni.caltech.edu>
2010-12-11 09:06:44 -0800

    Make tooltips less chatty, and delete unused text.

--- strings.h
+++ strings.h
@@ -136,58 +136,30 @@ gchar Aheader[]  = "<b>Action</b>";
 /* 					TOOLTIPS 						*/
 /************************************************************************************************/
 
-gchar setx1tts[] =	"Put out first point on x axis";
-gchar setx1tt[] =	"To set out first point on x axis press this button and then press on the "
-			"image x-axis (with left mousebutton). This procedure can be replaced by "
-			"pressing the middle button on the image.";
-
-gchar setx2tts[] =	"Put out second point on x axis";
-gchar setx2tt[] =	"To set out second point on x axis press this button and then press on the "
-			"image x-axis (with left mousebutton). This procedure can be replaced by "
-			"pressing the middle button on the image.";
-
-gchar sety1tts[] =	"Put out first point on y axis";
-gchar sety1tt[] =	"To set out first point on y axis press this button and then press on the "
-			"image y-axis (with left mousebutton). This procedure can be replaced by "
-			"pressing the right button on the image.";
-
-gchar sety2tts[] =	"Put out second point on y axis";
-gchar sety2tt[] =	"To set out second point on y axis press this button and then press on the "
-			"image y-axis (with left mousebutton). This procedure can be replaced by "
-			"pressing the right button on the image.";
+gchar setx1tts[] =	"Set first point on x axis";
+gchar setx2tts[] =	"Set second point on x axis";
+gchar sety1tts[] =	"Set first point on y axis";
+gchar sety2tts[] =	"Set second point on y axis";
 
 gchar *setxytts[] = 	{&setx1tts[0], &setx2tts[0], &sety1tts[0], &sety2tts[0]};
-gchar *setxytt[] = 	{&setx1tt[0], &setx2tt[0], &sety1tt[0], &sety2tt[0]};
 
-gchar entryx1tt[] =	"This entry should contain the value of the first x-axis point";
-
-gchar entryx2tt[] =	"This entry should contain the value of the second x-axis point";
-
-gchar entryy1tt[] =	"This entry should contain the value of the first y-axis point";
-
-gchar entryy2tt[] =	"This entry should contain the value of the second y-axis point";
+gchar entryx1tt[] =	"Value of the first point on x axis";
+gchar entryx2tt[] =	"Value of the second point on x axis";
+gchar entryy1tt[] =	"Value of the first point on y axis";
+gchar entryy2tt[] =	"Value of the second point on y axis";
 
 gchar *entryxytt[] = 	{&entryx1tt[0], &entryx2tt[0], &entryy1tt[0], &entryy2tt[0]};
 
-gchar removeltt[] =	"This button removes the last put out point";
-
+gchar removeltt[] =	"Remove last point";
 gchar removeatts[] =	"Remove all points";
-gchar removeatt[] =	"This button removes all put out points except the axispoints. If all "
-			"points are already removed a second click will also remove the axispoints.";
 
-gchar printrestt[] =	"Print data to stdout";
+gchar printrestt[] =	"Export data to stdout";
+gchar printfilett[] =	"Export data to file %s";
+gchar filenamett[] =	"Filename to export data to";
 
-gchar printfilett[] =	"Print data to file %s";
-
-gchar filenamett[] =	"This entry should contain the name of the file to print data to";
-
-gchar logxtt[] =	"If x-axis is logarithmic check this button";
-
-gchar logytt[] =	"If y-axis is logarithmic check this button";
+gchar logxtt[] =	"X axis is logarithmic";
+gchar logytt[] =	"Y axis is logarithmic";
 
 gchar *logxytt[] = 	{&logxtt[0], &logytt[0]};
 
-gchar uetts[] = 	"Show the errors of the x and y values at printout";
-
-gchar uett[] =		"If you want the errors of the x value to be printed in column 3 and the error "
-			"of the y value to be printed in column 4 check this button.";
+gchar uetts[] = 	"Export errors of the x and y values";

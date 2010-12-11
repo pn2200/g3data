a3dae6ba2e46a0466992716d8bbac60c94ea9132
Paul Novak <pnovak@alumni.caltech.edu>
2010-12-11 09:11:11 -0800

    Quiet error message from update_preview_cb().
    Quiet error message:
    GdkPixbuf-CRITICAL **: gdk_pixbuf_new_from_file_at_scale: assertion `filename != NULL' failed
    from update_preview_cb() when a folder is selected in the
    GtkFileChooserDialog.

--- main.c
+++ main.c
@@ -755,21 +755,23 @@ gint InsertImage(char *filename, gdouble Scale, gdouble maxX, gdouble maxY, gint
 /****************************************************************/
 static void update_preview_cb (GtkFileChooser *file_chooser, gpointer data) {
   GtkWidget *preview;
-  char *filename;
+  gchar *filename;
   GdkPixbuf *pixbuf;
   gboolean have_preview;
 
     preview = GTK_WIDGET (data);
     filename = gtk_file_chooser_get_preview_filename (file_chooser);
+    if (filename != NULL) {
+        pixbuf = gdk_pixbuf_new_from_file_at_size (filename, 128, 128, NULL);
+        have_preview = (pixbuf != NULL);
+        g_free (filename);
 
-    pixbuf = gdk_pixbuf_new_from_file_at_size (filename, 128, 128, NULL);
-    have_preview = (pixbuf != NULL);
-    g_free (filename);
+        gtk_image_set_from_pixbuf (GTK_IMAGE (preview), pixbuf);
+        if (pixbuf)
+            g_object_unref (pixbuf);
 
-    gtk_image_set_from_pixbuf (GTK_IMAGE (preview), pixbuf);
-    if (pixbuf) g_object_unref (pixbuf);
-
-    gtk_file_chooser_set_preview_widget_active (file_chooser, have_preview);
+        gtk_file_chooser_set_preview_widget_active (file_chooser, have_preview);
+    }
 }
 
 
